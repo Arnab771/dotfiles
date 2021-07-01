d3de3736333b553554c20b25b94072a802799ebf
Arnab <arnab@pop-os.localdomain>
2021-07-01 16:14:56 +0530

    added line for initializing ssh agent

--- scripts/github-ssh-setup.sh
+++ scripts/github-ssh-setup.sh
@@ -3,6 +3,7 @@ echo "Installing xclip..."
 sudo apt install xclip
 ssh-keygen -t rsa -b 4096 -C "arnabchakraborty771@gmail.com"
 echo "Make sure to add a passphrase"
+eval "$(ssh-agent)"
 ssh-add ~/.ssh/id_rsa
 echo "The public is being copied to your clipboard..."
 xclip -sel clip < ~/.ssh/id_rsa.pub
