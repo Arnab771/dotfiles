21891f9412f36ff2fe229a0100f9bb440c01be08
Arnab <arnab@pop-os.localdomain>
2021-07-18 20:13:13 +0530

    enabled pdf previews

--- ranger/.config/ranger/scope.sh
+++ ranger/.config/ranger/scope.sh
@@ -66,7 +66,7 @@ handle_extension() {
 
         ## PDF
         pdf)
-            ## Preview as text conversion
+            ### Preview as text conversion
             pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - | \
               fmt -w "${PV_WIDTH}" && exit 5
             mutool draw -F txt -i -- "${FILE_PATH}" 1-10 | \
@@ -81,8 +81,7 @@ handle_extension() {
 
         ## OpenDocument
         odt|ods|odp|sxw)
-            ## Preview as text conversion
-            odt2txt "${FILE_PATH}" && exit 5
+            ## Preview as text conversion odt2txt "${FILE_PATH}" && exit 5
             ## Preview as markdown conversion
             pandoc -s -t markdown -- "${FILE_PATH}" && exit 5
             exit 1;;
@@ -160,14 +159,14 @@ handle_image() {
              exit 1;;
 
         ## PDF
-        # application/pdf)
-        #     pdftoppm -f 1 -l 1 \
-        #              -scale-to-x "${DEFAULT_SIZE%x*}" \
-        #              -scale-to-y -1 \
-        #              -singlefile \
-        #              -jpeg -tiffcompression jpeg \
-        #              -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
-        #         && exit 6 || exit 1;;
+         application/pdf)
+             pdftoppm -f 1 -l 1 \
+                      -scale-to-x "${DEFAULT_SIZE%x*}" \
+                      -scale-to-y -1 \
+                      -singlefile \
+                      -jpeg -tiffcompression jpeg \
+                      -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
+                 && exit 6 || exit 1;;
 
 
         ## ePub, MOBI, FB2 (using Calibre)
